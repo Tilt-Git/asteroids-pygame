4edd6210925ac51ce952ea1d64187d233917248b
Tilt-Git <likelyontilt@gmail.com>
2026-09-17 03:08:21 -0400

    B: added core movement functionality and screen

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from player import Player
 from logger import log_state
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0.0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH} \n"
@@ -14,7 +18,11 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.update(dt)
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+        
 
 
 if __name__ == "__main__":
